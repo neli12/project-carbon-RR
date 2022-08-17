93b0491cd1b772d1e06eaa71726f73eed825050e
Neli <neliquinhonez92@gmail.com>
2022-08-17 12:10:58 -0300

    Add files via upload

--- script_geo.R
+++ script_geo.R
@@ -3,23 +3,22 @@ library(gstat)
 library(sp)
 library(raster)
 library(hydroGOF)
-library(leaflet)
 library(sf)
-library("dplyr")
-library(ggplot2)
-library(patchwork)
-library(viridis)
+library(rgdal)
+
 
 # Set working directory and load files
 setwd("C:/Users/neliq/Documents/NIF/Rothamsted/datasets_RR")
+getwd()
 list.files()
 
-field_2016 <- read.csv("field_2016.csv", sep = ';')
-colnames(field_2016) <- c('Experiment',"SP_ID", "sample_distance_m", "Field", "Sample_date", 
-                          "Sample_time", 'X', 'Y', "pH", "BD_gcm3", "OM%", "N_total%", "C_total",
+field_2016 <- read.csv("files_2016-07-01.csv", sep = ';')
+colnames(field_2016) <- c('Experiment',"SP_ID", "sample_distance_m", "Field", "Sample_date", 'Month.year', 'Year',
+                          "Sample_time", 'X', 'Y','Lat', 'Long', "pH", "BD_gcm3", "OM%", "N_total%", "C_total",
                           "conductivity_uScm")
 
 hist(field_2016$C_total)
+boxplot(field_2016$C_total)
 
 
 # Set dataset as spatial object
@@ -29,8 +28,9 @@ plot(field_2016)
 spplot(field_2016["C_total"], main = "C%")
 
 ## Fit universal kriging 
-g <- gstat(id="C_total%", formula = C_total~X+Y, data = field_2016)
+g <- gstat(id="C_total", formula = C_total~X+Y, data = field_2016)
 variog <- gstat::variogram(g)
+variog
 plot(variog, pch=16, cex=1)
 
 
@@ -43,10 +43,10 @@ plot(variog, fit_shp, pch=16, cex=1)
 fit_shp
 
 
-## Cross-validation
+## Cross-validation - 10-fold
 # Exponential
 crossval_exp <- krige.cv(C_total~X+Y, locations = field_2016, model = fit_exp, nfold=10)
-plot(crossval_exp$var1.pred ~ field_2016$C_total, cex = 1.2, lwd = 2, xlim = c(2,10), ylim = c(2,10))
+plot(crossval_exp$var1.pred ~ field_2016$C_total, cex = 1.2, lwd = 2, xlim = c(0,10), ylim = c(0,10))
 abline(0, 1, col = "red", lwd = 2)
 lm_exp <- lm(crossval_exp$var1.pred ~ field_2016$C_total)
 abline(lm_exp, col = "green", lwd = 2)
@@ -72,7 +72,7 @@ bubble(crossval_shp , "residual", main = "C% 5-fold CV residuals - Spherical")
 
 # Kriging
 # Convert to sf file
-points_sf <- st_as_sf(field_2016, coords = c("X", "Y"), crs = 32630) %>% 
+points_sf <- st_as_sf(field_2016, coords = c("X", "Y"), crs = 27700) %>% 
   cbind(st_coordinates(.))
 
 # Create a grid of 5x5m
@@ -88,10 +88,18 @@ grd_5m_sp <- as(grd_5m_sf, "Spatial")
 gridded(grd_5m_sp) <- TRUE           
 grid <- as(grd_5m_sp , "SpatialPixels")
 colnames(grid@coords) <- c('X', 'Y')
+plot(grid)
+
+## Or load from computer
+grid <- read.csv("grid_5m.csv", sep = ';')[,2:3]
+colnames(grid)<- c("X", "Y")
+gridded(grid) = ~X+Y
+plot(grid)
 
 # Exponential kriging
 predicted_exp <- krige(formula = C_total~X+Y, locations = field_2016, newdata = grid, model = fit_exp)
 plot(predicted_exp)
+predicted_exp
 
 # Spherical kriging
 predicted_shp <- krige(formula = C_total~X+Y, locations = field_2016, newdata = grid, model = fit_shp)
@@ -106,5 +114,5 @@ predicted_raster_shp <- raster(predicted_shp)
 plot(predicted_raster_shp, main = 'Kriging - Spherical')
 
 ## Export raster
-writeRaster(predicted_raster_shp, 'predicted_2016.tif')
+writeRaster(predicted_raster_shp, 'predicted_2016.tif', overwrite = TRUE)
 
