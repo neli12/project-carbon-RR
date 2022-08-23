1831ef029ae47eb4d6ae7a587ab5a8a42e9b939a
Neli <neliquinhonez92@gmail.com>
2022-08-23 12:26:24 -0300

    Add files via upload

--- script_shape_to_raster_and_dummy.R
+++ script_shape_to_raster_and_dummy.R
@@ -4,19 +4,25 @@ library(rgdal)
 
 # Set working directory and load shapefile and raster
 setwd('C:/Users/neliq/Documents/NIF/Rothamsted')
-shp <- shapefile('shapefiles/soil_layer.shp')
-img <- stack("satellites_images/Sentinel/2016-07-19.tif")
+shp_soil <- shapefile('shapefiles/soil_layer.shp')
+shp_field <- shapefile('shapefiles/field.shp')
+img <- stack("satellites_images/Landsat/2011-04-21.tif")
 
 # Convert character to numeric
-shp@data$Soil_Code <- as.numeric(shp@data$Soil_Code)
+shp_soil@data$Soil_Code <- as.numeric(shp_soil@data$Soil_Code)
+shp_field@data$code_field <- as.numeric(shp_field@data$code_field)
 
 # Shapefile to raster
-shp_to_raster <- rasterize(shp, img, 'Soil_Code')
-plot(shp_to_raster)
+soil_to_raster <- rasterize(shp_soil, img, 'Soil_Code')
+field_to_raster <- rasterize(shp_field, img, 'code_field')
+plot(soil_to_raster)
+plot(field_to_raster)
 
 # Check extent and export raster
-extent(shp_to_raster) == extent(img)  
-writeRaster(shp_to_raster, 'rasters/soil_raster.tif')
+extent(soil_to_raster) == extent(img)  
+extent(field_to_raster) == extent(img)
+writeRaster(soil_to_raster, 'rasters/fieldL5_raster.tif', overwrite=TRUE)
+writeRaster(field_to_raster, 'rasters/soilL5_raster.tif', overwrite=TRUE)
 
 # Convert raster to dummy
 dummyRaster <- function(rast){
@@ -31,13 +37,26 @@ dummyRaster <- function(rast){
 }
 
 
-soil_dummy <- dummyRaster(shp_to_raster)
+# Apply dummy function and rename
+soil_dummy <- dummyRaster(soil_to_raster)
+field_dummy <- dummyRaster(field_to_raster)
 names(soil_dummy) <- c("Dg_Chb", 'Fa', 'Hk', 'Hw')
-plot(soil_dummy)
+
+names_field <- levels(sort(factor(shp_field@data$Field_Name)))
+names(field_dummy) <- names_field
+plot(field_dummy)
 
 
-# Stack image and new soil dummy raster
-covs_dummy <- stack(img, soil_dummy)
-plot(covs_dummy)
+# Join image and new dummys and export
 
-writeRaster(covs_dummy, 'rasters/covs_s2a_soil_dummy.tif')
+covs <- stack(img, soil_dummy, field_dummy)
+names(covs )
+
+writeRaster(soil_dummy, 'rasters/soilS2A_dummy.tif', overwrite=TRUE)
+writeRaster(field_dummy, 'rasters/fieldS2A_dummy.tif', overwrite=TRUE)
+writeRaster(covs, 'rasters/covsS2A_soil_field.tif', overwrite=TRUE)
+
+
+writeRaster(soil_dummy, 'rasters/soilL5_dummy.tif' , overwrite=TRUE)
+writeRaster(field_dummy, 'rasters/fieldL5_dummy.tif', overwrite=TRUE)
+writeRaster(covs, 'rasters/covsL5_soil_field_1.tif', overwrite=TRUE)
