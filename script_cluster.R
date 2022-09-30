a875cfaf0cec777115837f65cc669ff33fad3b6e
Neli <neliquinhonez92@gmail.com>
2022-09-30 09:57:22 -0300

    Add files via upload

---
+++ script_cluster.R
@@ -0,0 +1,91 @@
+### Script for mapping clusters using raster maps
+
+## Load libraries
+library(raster)
+library(cluster)
+
+## Set directory
+setwd("C:/Users/neliq/Documents/NIF/Rothamsted/test-prediction-2012")
+
+# Load raster stack 
+L8_terrain <- stack("L8Terrain5m_DEF.tif")
+
+# Get the values of the raster stack
+stack_vals <- getValues(L8_terrain)
+summary(stack_vals)
+
+# Elbow method to check the optimal number of cluster
+k.max <- 20
+data <- (na.omit(scale(stack_vals))) #scale and exclude NA's
+wss <- sapply(1:k.max, 
+              function(k){kmeans(data, k, nstart=50,iter.max = 15 )$tot.withinss})
+wss
+plot(wss)
+
+
+
+### Clustering
+# Use the clara function from cluster package. In this code, a sample of 1000 and the euclidean distance are used.
+# I used k=10, as example
+clus <- cluster::clara(data, k=5, metric = "euclidean", stand = TRUE, samples = 1000, pamLike = T)  
+summary(clus)
+
+
+# Create am index
+idx <- 1:ncell(L8_terrain)
+idx <- idx[-unique(which(is.na(stack_vals), arr.ind=TRUE)[,1])] 
+str(idx)
+
+# Create an empty raster using the first raster of your stack
+clust_raster <- L8_terrain[[1]]
+plot(clust_raster)
+clust_raster[] <- NA
+
+# Transfer the clustering results to your empty raster and save it
+library(RColorBrewer)
+clust_raster[idx] <- clus$clustering
+cols <- brewer.pal(5, 'RdYlGn')
+cols <- brewer.pal(10, 'Spectral')
+cols <- brewer.pal(10, 'Set3')
+
+cols
+pal <- colorRampPalette(c("#A50026",
+                          "#FFFFB3",
+                          "#006837",
+                          "#5E4FA2",
+                          "#FEE08B",
+                          "#F46D43",
+                          "#BC80BD",
+                          "#FDB462",
+                          "#66C2A5",
+                          "#FB8072"))
+plot(clust_raster, col = cols) 
+crs(clust_raster)
+
+writeRaster(clust_raster, "Clusters_5", format = "GTiff", datatype = "FLT4S", overwrite = T)
+
+
+
+## Load libraries
+library(raster)
+library(stats)
+
+## Take a sample
+sr <- sampleRandom(L8_terrain, 1000)
+
+## Perform PCA
+pca <- prcomp(sr, scale = TRUE)
+pca.df <- pca$x
+print(pca)
+
+## Plot the screeplot
+screeplot(pca)
+
+## Predict to the entire raster   -- With three PCs
+pci <- predict(L8_terrain, pca, index = 1:3)
+plot(pci)
+
+
+## Save
+writeRaster(pci, "PCA_L8terrain.tif")
+
