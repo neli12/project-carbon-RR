46a5e493c50ff8b38e209ebf71fc19a4ae0ee7b3
Neli <neliquinhonez92@gmail.com>
2022-10-12 07:48:57 -0300

    Create weather-data.sql

---
+++ weather-data.sql
@@ -0,0 +1,389 @@
+# Table 2021
+CREATE TABLE nwp.measurements (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+ 
+# Table 2011
+CREATE TABLE nwp.Y2011 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+    
+CREATE TABLE nwp.Y2012 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);    
+
+CREATE TABLE nwp.Y2013 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+    
+CREATE TABLE nwp.Y2014 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+
+CREATE TABLE nwp.Y2015 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+
+CREATE TABLE nwp.Y2016 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+
+CREATE TABLE nwp.Y2017 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+
+CREATE TABLE nwp.Y2018 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+
+CREATE TABLE nwp.Y2019 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+
+CREATE TABLE nwp.Y2020 (
+	`date_time` TEXT DEFAULT NULL,
+    `prec_mm` DOUBLE DEFAULT NULL,
+    `prec_quality` TEXT DEFAULT NULL,
+    `prec_last` TEXT DEFAULT NULL,
+    `airT` DOUBLE DEFAULT NULL,
+    `airT_quality` TEXT DEFAULT NULL,
+    `airT_last` TEXT DEFAULT NULL,
+	`RH` DOUBLE DEFAULT NULL,
+    `RH%_quality` TEXT DEFAULT NULL,
+    `RH%_last` TEXT DEFAULT NULL,
+	`wind_speed` DOUBLE DEFAULT NULL,
+    `wind_speed_quality` TEXT DEFAULT NULL,
+    `wind_speed_last` TEXT DEFAULT NULL,
+	`wind_dir` DOUBLE DEFAULT NULL,
+    `wind_dir_quality` TEXT DEFAULT NULL,
+    `wind_dir_last` TEXT DEFAULT NULL,
+	`solar_rad` DOUBLE DEFAULT NULL,
+    `solar_rad_quality` TEXT DEFAULT NULL,
+    `solar_rad_last` TEXT DEFAULT NULL);
+
+## Add data by command line
+# mysql --local-infile=1 -u root -p
+# senha
+# SET GLOBAL local_infile=true;
+
+# New table 2021
+CREATE TABLE nwp.meas2021 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.measurements
+GROUP BY data_inicio1);
+
+# New table 2011
+CREATE TABLE nwp.meas2011 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2011
+GROUP BY data_inicio1);
+
+CREATE TABLE nwp.meas2012 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2012
+GROUP BY data_inicio1);
+
+# 2013
+CREATE TABLE nwp.meas2013 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2013
+GROUP BY data_inicio1);
+
+# 2014
+CREATE TABLE nwp.meas2014 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2014
+GROUP BY data_inicio1);
+
+# 2015
+CREATE TABLE nwp.meas2015 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2015
+GROUP BY data_inicio1);
+
+# 2016
+CREATE TABLE nwp.meas2016 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2016
+GROUP BY data_inicio1);
+
+# 2017
+CREATE TABLE nwp.meas2017 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2017
+GROUP BY data_inicio1);
+
+# 2018
+CREATE TABLE nwp.meas2018 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2018
+GROUP BY data_inicio1);
+
+# 2019
+CREATE TABLE nwp.meas2019 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2019
+GROUP BY data_inicio1);
+
+# 2020
+CREATE TABLE nwp.meas2020 (
+SELECT CAST(date_time as date) AS data_inicio1,
+(ROUND(AVG(prec_mm) OVER (ORDER BY CAST(date_time as date)),2)/10) as avg_prec,
+ROUND(MIN(airT) OVER (ORDER BY CAST(date_time as date)),2) as min_airT,
+ROUND(MAX(airT) OVER (ORDER BY CAST(date_time as date)),2) as max_airT,
+ROUND(AVG(RH) OVER (ORDER BY CAST(date_time as date)),2) as avg_rh,
+ROUND((AVG(wind_speed) OVER (ORDER BY CAST(date_time as date))*0.621371),2) as avg_wind_speed,
+ROUND(AVG(wind_dir) OVER (ORDER BY CAST(date_time as date)),2) as avg_wind_dir,
+ROUND((AVG(solar_rad) OVER (ORDER BY CAST(date_time as date))*2.0636299664),2) as avg_solar_rad
+FROM `nwp`.y2020
+GROUP BY data_inicio1);
+
+### Split date
+CREATE TABLE nwp.def2012(
+SELECT 
+	DAY(data_inicio1) AS day2012,
+	MONTH(data_inicio1) AS month2012,
+	YEAR(data_inicio1) AS year2012,
+    ROW_NUMBER() OVER (ORDER BY data_inicio1) AS dayoftheyear,
+    max_airT, min_airT, ROUND(avg_prec, 5) as avg_prec, avg_solar_rad, avg_wind_speed, avg_rh 
+FROM nwp.meas2012);
